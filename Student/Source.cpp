3615e78d24a8744f8e1f9953f7d7aa1ff41debb2
Алишер Борангазиев <@local.itstep>
2019-04-22 22:00:01 +0600

    Добавьте файлы проекта.

---
+++ Student/Source.cpp
@@ -0,0 +1,27 @@
+#include"student.h"
+
+void main()
+{
+	student x;
+	student h;
+
+	//h = x;
+
+	//cout << h;
+
+	//x.setName("Petor");
+	//x.setGroup(2);
+	//x.setGrades(5);
+
+	cin >> x;
+
+
+
+	cout << x;
+
+	x.addMark(5);
+
+	cout << x;
+
+	system("pause");
+}
